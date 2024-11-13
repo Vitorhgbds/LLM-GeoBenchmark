fb76b18a6295b431ecfdbeee3a9e9bb19e412f4a
Vitorhgbds <vitor_hgbds@hotmail.com>
2024-11-13 03:43:16 -0300

    added commons for path handling

---
+++ commons.py
@@ -0,0 +1,8 @@
+BASE_PATH_LLAMA_3_2_1B          = "C:\\Users\\vitor\\.llama\\checkpoints\\Llama3.2-1B"
+PATH_CHECKLIST_LLAMA_3_2_1B     = f"{BASE_PATH_LLAMA_3_2_1B}\\checklist.chk"
+PATH_TOKENIZER_LLAMA_3_2_1B     = f"{BASE_PATH_LLAMA_3_2_1B}\\tokenizer.model"
+PATH_PARAMS_LLAMA_3_2_1B        = f"{BASE_PATH_LLAMA_3_2_1B}\\params.json"
+PATH_CONSOLIDATED_LLAMA_3_2_1B  = f"{BASE_PATH_LLAMA_3_2_1B}\\consolidated.00.pth"
+
+
+

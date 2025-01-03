448bb15367928073b9723b0e4f50cd40e889a459
vitorhgbds <vitor_hgbds@hotmail.com>
2025-01-03 14:05:22 -0300

    chore(params): changing model params

--- gas/commons.py
+++ gas/commons.py
@@ -1,12 +1,12 @@
 SEED = 42
 GPT_JUDGE = "gpt-4o-mini"
 
-MAX_LENGHT = 500  # Adjust for shorter output
+MAX_NEW_TOKENS = 1  # Adjust for shorter output
+MIN_NEW_TOKENS = 1  # Adjust for shorter output
 NO_REPEAT_NGRAM_SIZE = 2  # Prevent repetitive phrases
 REPETITION_PENALTY = 1.2  # Penalize repetition
-TEMPERATURE = None  # Control randomness
-TOP_K = None  # Top-k sampling
-TOP_P = None  # Nucleus sampling
-DO_SAMPLE = False  # Enable sampling
+TEMPERATURE = 0.2  # Control randomness
+TOP_K = 40  # Top-k sampling
+TOP_P = 0.75  # Nucleus sampling
+DO_SAMPLE = True  # Enable sampling
 TRUNCATION = True  # Ensure truncation to max_length
-RETURN_FULL_TEXT = False  # Avoid repeating the prompt

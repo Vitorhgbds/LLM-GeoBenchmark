075034127781aa54b510edd4c8d630ad12b46289
Vitorhgbds <vitor_hgbds@hotmail.com>
2025-04-03 14:27:26 -0300

    chore(tests): first dummy test case was added.

--- tests/test_custom_model.py
+++ tests/test_custom_model.py
@@ -1,28 +1,12 @@
-from deepeval.metrics import GEval
-from deepeval.test_case import LLMTestCaseParams
+from unittest.mock import MagicMock
+from gas.models import BaseModel
 
-from gas.models.llama import Llama3_8B
-
-correctness_metric = GEval(
-    name="Correctness",
-    model="gpt-4o-mini",
-    criteria="Determine whether the actual output is factually correct based on the expected output.",
-    # NOTE: you can only provide either criteria or evaluation_steps, and not both
-    evaluation_steps=[
-        "Check whether the facts in 'actual output' contradicts any facts in 'expected output'",
-        "You should also heavily penalize omission of detail",
-        "Vague language, or contradicting OPINIONS, are OK",
-    ],
-    evaluation_params=[
-        LLMTestCaseParams.INPUT,
-        LLMTestCaseParams.ACTUAL_OUTPUT,
-        LLMTestCaseParams.EXPECTED_OUTPUT,
-    ],
-)
-
-
-def test_llama3_8B():
-    llama = Llama3_8B()
+def test_base_model():
+    # Mock base model
+    GENERATED_OUTPUT = "This is a test output."
+    mock_base_model = MagicMock(spec=BaseModel)
+    mock_base_model.generate.return_value = GENERATED_OUTPUT
+    
     question = "The dog chased the cat up the tree, who ran up the tree?"
-    actual_output = llama.generate(question)
+    actual_output = mock_base_model.generate(question)
     assert len(actual_output) > 0
